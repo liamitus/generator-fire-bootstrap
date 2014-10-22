ef1699c421217f52025f7128d6d77b454100a7a3
Liam Howell <lhowell@mobiquityinc.com>
2014-10-22 16:11:02 -0400

    Receives config from parent generator

--- app/index.js
+++ app/index.js
@@ -18,6 +18,16 @@ var chalk = require("chalk");
 
 module.exports = yeoman.generators.Base.extend({
 
+	constructor: function () {
+		// Call the super constructor.
+		yeoman.generators.Base.apply(this, arguments);
+
+		if (arguments && arguments[0]) {
+			var configJSON = JSON.parse(arguments[0]);
+			this.config.set("project_config", configJSON);
+		}
+	},
+
 	welcomeMessage: function () {
 		this.log("Adding Bootstrap...");
 	}
