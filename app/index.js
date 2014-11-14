00c09da4bc6bbfb9530be4a45fa9b16c462ba5b8
Liam Howell <lhowell@mobiquityinc.com>
2014-11-14 13:31:48 -0500

    Adds the new grunt config without overriding

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@
 	by adding a Gruntfile task.
 
 	Author:	Liam Howell <lhowell@mobiquityinc.com>
-	Since:	10-30-2014
+	Since:	11-14-2014
 */
 
 // --------------------------------------------------------------------- Imports
@@ -15,6 +15,9 @@
 var yeoman = require("yeoman-generator");
 var GruntfileEditor = require("gruntfile-editor");
 var chalk = require("chalk");
+var fs = require("fs");
+var utils = require("gruntfile-editor-utils");
+var _ = require("lodash");
 
 // ----------------------------------------------------------------- Main action
 
@@ -23,6 +26,8 @@ module.exports = yeoman.generators.Base.extend({
 	constructor: function () {
 		// Call the super constructor.
 		yeoman.generators.Base.apply(this, arguments);
+
+		// this.log("Bootstrap constructor running...");
 
 		if (arguments && arguments[0]) {
 			var configJSON = JSON.parse(arguments[0]);
@@ -41,7 +46,7 @@ module.exports = yeoman.generators.Base.extend({
 
 		var bs = "bootstrap";
 		bower.dependencies[bs] = "~3.2.0";
-		
+
 		this.write("bower.json", JSON.stringify(bower, null, 2));
 	},
 
@@ -49,31 +54,56 @@ module.exports = yeoman.generators.Base.extend({
 		installBootstrap: function () {
 			var done = this.async();
 			
-
 			// Retrieve the exiting gruntfile or make a new one.
 			// TODO the same thing for gulp?
-			// if (this.config.get("project_config").build_system === "grunt") {
-			// 	this.log(chalk.yellow("Adding Bootstrap to existing Gruntfile..."));
-			// 	// Get the gruntfile.
-			// 	var gruntFileContent = this.dest.read("Gruntfile.js");
-			// 	// Pass it to the GruntfileEditor and overwrite this.env.gruntfile 
-			// 	this.env.gruntfile = new GruntfileEditor(gruntFileContent);
+			if (this.config.get("project_config").build_system === "grunt") {
+				
+				this.log(chalk.yellow("Adding Bootstrap to existing Gruntfile..."));
+				
+				// Get the gruntfile.
+				var gruntfileContent = this.dest.read("Gruntfile.js");
+				
+			    // Pass it to the GruntfileEditor and overwrite this.env.gruntfile 
+				this.env.gruntfile = new GruntfileEditor(gruntfileContent);                
 
-			// 	// Use the GruntfileEditor API to programmatically add tasks
-			// 	// this.gruntfile.insertConfig('watch', "......");
-			// 	// this.gruntfile.insertConfig('clean', "......");
-			// } else {
-			// 	// Install bootstrap.css directly.
-			// 	this.log(chalk.yellow("Installing Bootstrap..."));
-			// 	this.src.copy("bootstrap.v3.2.0.min.css", "app/bootstrap.v3.2.0.min.css");
-			// }
+				var copyBootstrap = {
+					expand: true,
+					dot: true,
+					cwd: "bower_components/bootstrap/dist",
+					src: "css",
+					dest: "<%= config.dist %>"
+				};
+				
+                // Extract the copy property from gruntfile
+                var extractedCopyProp = utils.extractJSON(gruntfileContent, "copy");
+
+                // Add the extracted copy property (if it exists) to the new one.
+                if (extractedCopyProp) {
+                    extractedCopyProp = JSON.parse(extractedCopyProp);
+                    copyBootstrap = _.merge(copyBootstrap, extractedCopyProp);
+                }
+                
+				var copyBootstrapString = JSON.stringify(copyBootstrap, null, 2);
+
+				// Use the GruntfileEditor API to programmatically add tasks
+				this.gruntfile.insertConfig("copy", copyBootstrapString);
+
+				fs.writeFileSync("Gruntfile.js", this.env.gruntfile.toString());
+				
+			} else {
+				// Install bootstrap.css directly.
+				this.log(chalk.yellow("Installing Bootstrap..."));
+				this.src.copy("../../bower_components/bootstrap/dist/css/bootstrap.min.css", "app/bootstrap.v3.2.0.min.css");
+			}
 
 
 			// bower.commands
 			// .install(["bootstrap"], { exclude: "bower_components/jquery/dist/jquery.js" })
 			// .on("end", function () {
 			// 	console.log("Latest Bootstrap successfully installed.");
+
 				done();
+			
 			// });
 		}
 		
