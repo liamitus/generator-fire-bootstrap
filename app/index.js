f7dde848ba6da72bc1e3815f53aefb4a12a0552c
Liam Howell <lhowell@mobiquityinc.com>
2014-11-24 14:24:36 -0500

    Bootstrap now adds the file and updates index.html
    if not using grunt or equivalent build tool

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@
 	by adding a Gruntfile task.
 
 	Author:	Liam Howell <lhowell@mobiquityinc.com>
-	Since:	11-14-2014
+	Since:	11-20-2014
 */
 
 // --------------------------------------------------------------------- Imports
@@ -18,10 +18,121 @@ var chalk = require("chalk");
 var fs = require("fs");
 var utils = require("gruntfile-editor-utils");
 var _ = require("lodash");
+var jsdom = require("jsdom");
+
+// ----------------------------------------------- Private Methods and Variables
+
+var BootstrapGenBase = yeoman.generators.Base.extend({
+
+    settings: {
+        indexPage: "app/index.html",
+        bootstrapFilename: "bootstrap.min.css",
+        bootstrapSource: function () { return "bower_components/bootstrap/dist/css/" + this.bootstrapFilename},
+        bootstrapDestination: function () { return "app/" + this.bootstrapFilename; }
+    },
+
+    /**
+     * Add the lines to an existing gruntfile to load bootstrap dynamically.
+     */
+    addToGruntfile: function () {
+        this.log(chalk.yellow("Adding Bootstrap to existing Gruntfile..."));
+        
+        // Get the gruntfile.
+        var gruntfileContent = this.dest.read("Gruntfile.js");
+        
+        // Pass it to the GruntfileEditor and overwrite this.env.gruntfile 
+        this.env.gruntfile = new GruntfileEditor(gruntfileContent);                
+
+        var copyBootstrap = {
+            expand: true,
+            dot: true,
+            cwd: "bower_components/bootstrap/dist",
+            src: "css",
+            dest: "<%= config.dist %>"
+        };
+        
+        // Extract the copy property from gruntfile
+        var extractedCopyProp = utils.extractJSON(gruntfileContent, "copy");
+
+        // Add the extracted copy property (if it exists) to the new one.
+        if (extractedCopyProp) {
+            extractedCopyProp = JSON.parse(extractedCopyProp);
+            copyBootstrap = _.merge(copyBootstrap, extractedCopyProp);
+        }
+        
+        var copyBootstrapString = JSON.stringify(copyBootstrap, null, 2);
+
+        // Use the GruntfileEditor API to programmatically add tasks
+        this.gruntfile.insertConfig("copy", copyBootstrapString);
+
+        fs.writeFileSync("Gruntfile.js", this.env.gruntfile.toString());
+        
+    },
+
+    /**
+     * Add the Bootstrap css file to the project (if not using grunt or gulp).
+     */
+    addBootstrapCssFile: function () {
+        var root = this.destinationRoot() + "/";
+        utils.copyFile(root + this.settings.bootstrapSource(), root + this.settings.bootstrapDestination());
+    },
+
+
+    /**
+     * Returns the doctype tag of the given window.document.
+     *
+     * Thanks to stackoverflow user Patrick McElhaney
+     * http://stackoverflow.com/a/12523515/1729686 
+     */
+    getDocTypeAsString: function (doc) { 
+        var node = doc.doctype;
+        return node ? "<!DOCTYPE "
+            + node.name
+            + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '')
+            + (!node.publicId && node.systemId ? ' SYSTEM' : '') 
+            + (node.systemId ? ' "' + node.systemId + '"' : '')
+            + '>\n' : '';
+    },
+
+    /**
+     * Add the bootstrap css link to the index.html file.
+     */
+    addBootstrapToHTML: function () {
+        var root = this.destinationRoot() + "/";
+        var doctype = this.getDocTypeAsString;
+        var indexPage = root + this.settings.indexPage;
+        var indexHTMLString;
+        jsdom.env(indexPage, ["http://code.jquery.com/jquery.js"], function (errors, window) { 
+            var $head = window.$("head");
+            var $lastLink = $head.find("link:last");
+            var linkElement = "\t<link rel='stylesheet' href='bootstrap.min.css' type='text/css' media='screen'>";
+            if ($lastLink.length) {
+                $lastLink.after("\n" + linkElement);
+            } else {
+                $head.append(linkElement + "\n");
+            }
+            // This gets added by jsdom and is not needed/wanted.
+            window.$(".jsdom").remove(); 
+            indexHTMLString = doctype(window.document) + window.document.documentElement.outerHTML; 
+            fs.writeFileSync(indexPage, indexHTMLString);
+        });
+        console.log("Added bootstrap script to index.html");
+    },
+
+    /**
+     * Adds the bootstrap file to the project, and adds a script tag to the
+     * index.html page.
+     */
+    addBootstrapCss: function () {
+        this.addBootstrapCssFile();
+        this.addBootstrapToHTML();
+    }
+
+});
 
 // ----------------------------------------------------------------- Main action
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = BootstrapGenBase.extend({
 
 	constructor: function () {
 		// Call the super constructor.
@@ -58,43 +169,14 @@ module.exports = yeoman.generators.Base.extend({
 			// TODO the same thing for gulp?
 			if (this.config.get("project_config").build_system === "grunt") {
 				
-				this.log(chalk.yellow("Adding Bootstrap to existing Gruntfile..."));
-				
-				// Get the gruntfile.
-				var gruntfileContent = this.dest.read("Gruntfile.js");
-				
-			    // Pass it to the GruntfileEditor and overwrite this.env.gruntfile 
-				this.env.gruntfile = new GruntfileEditor(gruntfileContent);                
-
-				var copyBootstrap = {
-					expand: true,
-					dot: true,
-					cwd: "bower_components/bootstrap/dist",
-					src: "css",
-					dest: "<%= config.dist %>"
-				};
-				
-                // Extract the copy property from gruntfile
-                var extractedCopyProp = utils.extractJSON(gruntfileContent, "copy");
-
-                // Add the extracted copy property (if it exists) to the new one.
-                if (extractedCopyProp) {
-                    extractedCopyProp = JSON.parse(extractedCopyProp);
-                    copyBootstrap = _.merge(copyBootstrap, extractedCopyProp);
-                }
-                
-				var copyBootstrapString = JSON.stringify(copyBootstrap, null, 2);
-
-				// Use the GruntfileEditor API to programmatically add tasks
-				this.gruntfile.insertConfig("copy", copyBootstrapString);
-
-				fs.writeFileSync("Gruntfile.js", this.env.gruntfile.toString());
+				this.addToGruntfile();
 				
 			} else {
 				// Install bootstrap.css directly.
 				this.log(chalk.yellow("Installing Bootstrap..."));
-				this.src.copy("../../bower_components/bootstrap/dist/css/bootstrap.min.css", "app/bootstrap.v3.2.0.min.css");
-			}
+                this.addBootstrapCss();
+
+            }
 
 
 			// bower.commands
